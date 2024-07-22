32fa568feca0748a3683fbcbab3a3f1c6d7db930
AdithyaShanker99 <82780565+AdithyaShanker99@users.noreply.github.com>
2024-07-22 12:39:14 -0700

    lighting

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,44 +1,118 @@
-#include "../Headers/Camera.h"
+#include"../Headers/Camera.h"
+
+
 
 Camera::Camera(int width, int height, glm::vec3 position)
 {
-    Camera::width = width;
-    Camera::height = height;
-    eye = position;
+	Camera::width = width;
+	Camera::height = height;
+	Position = position;
 }
 
-void Camera::Matrix(float FOVdeg, float clipNear, float clipFar, Shader& shader, const char* uniformName)
+void Camera::updateMatrix(float FOVdeg, float nearPlane, float farPlane)
 {
-    glm::mat4 model = glm::mat4(1.0f);
-    glm::mat4 view = glm::mat4(1.0f);
-    glm::mat4 proj = glm::mat4(1.0f);
+	// Initializes matrices since otherwise they will be the null matrix
+	glm::mat4 view = glm::mat4(1.0f);
+	glm::mat4 projection = glm::mat4(1.0f);
 
-    view = glm::lookAt(eye,center,up);
-    proj = glm::perspective(glm::radians(FOVdeg), (float)width/height, clipNear, clipFar);
+	// Makes camera look in the right direction from the right position
+	view = glm::lookAt(Position, Position + Orientation, Up);
+	// Adds perspective to the scene
+	projection = glm::perspective(glm::radians(FOVdeg), (float)width / height, nearPlane, farPlane);
 
-
-    glUniformMatrix4fv(glGetUniformLocation(shader.ID, uniformName), 1 , GL_FALSE, glm::value_ptr(proj*view*model));
+	// Sets new camera matrix
+	cameraMatrix = projection * view;
 }
 
-void Camera::input_callback(GLFWwindow* window)
+void Camera::Matrix(Shader& shader, const char* uniform)
 {
-    if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-    {
-        eye+=speed*center;
-    }
-    if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-    {
-        eye+=speed*center;
-    }
-    if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-    {
-        eye+=speed*-center;
-    }
-    if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-    {
-        glm::vec3 right = glm::normalize(glm::cross(center, up));
-        eye += -speed * right;
+	// Exports camera matrix
+	glUniformMatrix4fv(glGetUniformLocation(shader.ID, uniform), 1, GL_FALSE, glm::value_ptr(cameraMatrix));
+}
 
-    }
 
+
+void Camera::Inputs(GLFWwindow* window)
+{
+	// Handles key inputs
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+	{
+		Position += speed * Orientation;
+	}
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+	{
+		Position += speed * -glm::normalize(glm::cross(Orientation, Up));
+	}
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+	{
+		Position += speed * -Orientation;
+	}
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+	{
+		Position += speed * glm::normalize(glm::cross(Orientation, Up));
+	}
+	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+	{
+		Position += speed * Up;
+	}
+	if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS)
+	{
+		Position += speed * -Up;
+	}
+	if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+	{
+		speed = 0.4f;
+	}
+	else if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_RELEASE)
+	{
+		speed = 0.1f;
+	}
+
+
+	// Handles mouse inputs
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
+	{
+		// Hides mouse cursor
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+
+		// Prevents camera from jumping on the first click
+		if (firstClick)
+		{
+			glfwSetCursorPos(window, (width / 2), (height / 2));
+			firstClick = false;
+		}
+
+		// Stores the coordinates of the cursor
+		double mouseX;
+		double mouseY;
+		// Fetches the coordinates of the cursor
+		glfwGetCursorPos(window, &mouseX, &mouseY);
+
+		// Normalizes and shifts the coordinates of the cursor such that they begin in the middle of the screen
+		// and then "transforms" them into degrees 
+		float rotX = sensitivity * (float)(mouseY - (height / 2)) / height;
+		float rotY = sensitivity * (float)(mouseX - (width / 2)) / width;
+
+		// Calculates upcoming vertical change in the Orientation
+		glm::vec3 newOrientation = glm::rotate(Orientation, glm::radians(-rotX), glm::normalize(glm::cross(Orientation, Up)));
+
+		// Decides whether or not the next vertical Orientation is legal or not
+		if (abs(glm::angle(newOrientation, Up) - glm::radians(90.0f)) <= glm::radians(85.0f))
+		{
+			Orientation = newOrientation;
+		}
+
+		// Rotates the Orientation left and right
+		Orientation = glm::rotate(Orientation, glm::radians(-rotY), Up);
+
+		// Sets mouse cursor to the middle of the screen so that it doesn't end up roaming around
+		glfwSetCursorPos(window, (width / 2), (height / 2));
+	}
+	else if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_RELEASE)
+	{
+		// Unhides cursor since camera is not looking around anymore
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+		// Makes sure the next time the camera looks around it doesn't jump
+		firstClick = true;
+	}
 }
--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,12 @@
 #include "../Headers/Textures.h"
 #include "../Headers/Camera.h"
 
+float fov = 45.0;
 
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    fov+=yoffset;
+}
 
 int main()
 {
@@ -30,18 +35,21 @@ int main()
     int width = 3840;
     int height = 2160;
 
-    // Vertices coordinates
-    GLfloat vertices[] =
+    // pyramidVertices coordinates
+    GLfloat pyramidVertices[] =
     { //     COORDINATES     /        COLORS      /   TexCoord  //
         -0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
         -0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
         0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	    0.0f, 0.0f,
         0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	    5.0f, 0.0f,
-        0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	    2.5f, 5.0f
+        0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	    2.5f, 5.0f,
+        //cube
+        
+
     };
 
-    // Indices for vertices order
-    GLuint indices[] =
+    // pyramidIndices for pyramidVertices order
+    GLuint pyramidIndices[] =
     {
         0, 1, 2,
         0, 2, 3,
@@ -49,6 +57,41 @@ int main()
         1, 2, 4,
         2, 3, 4,
         3, 0, 4
+    };
+
+
+    GLfloat lightVertices[] =
+    { //     COORDINATES     //
+        -0.1f, -0.1f,  0.1f,  // Vertex 0
+        -0.1f, -0.1f, -0.1f,  // Vertex 1
+        0.1f, -0.1f, -0.1f,   // Vertex 2
+        0.1f, -0.1f,  0.1f,   // Vertex 3
+        -0.1f,  0.1f,  0.1f,  // Vertex 4
+        -0.1f,  0.1f, -0.1f,  // Vertex 5
+        0.1f,  0.1f, -0.1f,   // Vertex 6
+        0.1f,  0.1f,  0.1f    // Vertex 7
+    };
+
+    GLuint lightIndices[] =
+    {
+        // Bottom face
+        0, 1, 2,
+        0, 2, 3,
+        // Front face
+        0, 4, 7,
+        0, 7, 3,
+        // Right face
+        3, 7, 6,
+        3, 6, 2,
+        // Back face
+        2, 6, 5,
+        2, 5, 1,
+        // Left face
+        1, 5, 4,
+        1, 4, 0,
+        // Top face
+        4, 5, 6,
+        4, 6, 7
     };
 
 
@@ -72,8 +115,8 @@ int main()
     VAO VAO1;
     VAO1.Bind();
 
-    VBO VBO1(vertices, sizeof(vertices));
-    EBO EBO1(indices, sizeof(indices));
+    VBO VBO1(pyramidVertices, sizeof(pyramidVertices));
+    EBO EBO1(pyramidIndices, sizeof(pyramidIndices));
 
     VBO1.Bind();
     EBO1.Bind();
@@ -87,6 +130,48 @@ int main()
     VAO1.Unbind();
     VBO1.Unbind();
     EBO1.Unbind();
+
+
+
+    Shader shaderLight("../Resources/Shaders/light.vert", "../Resources/Shaders/light.frag");
+
+    VAO VAO2;
+    VAO2.Bind();
+
+    VBO VBO2(lightVertices, sizeof(lightVertices));
+    EBO EBO2(lightIndices, sizeof(lightIndices));
+
+    VBO2.Bind();
+    EBO2.Bind();
+
+
+    VAO2.LinkAttrib(VBO2, 0, 3, GL_FLOAT, 3*sizeof(float), (void *)0);
+
+    VAO2.Unbind();
+    VBO2.Unbind();
+    EBO2.Unbind();
+
+    glm::vec4 lightColor = glm::vec4(1.0f, 1.0f,1.0f)
+
+    glm::vec3 lightPos = glm::vec3(0.5f,0.5f,0.5f);
+    glm::mat4 lightModel = glm::mat4(1.0f);
+    lightModel = glm::translate(lightModel, lightPos);
+
+    glm::vec3 pyramidPos = glm::vec3(0.0f,0.0f,0.0f);
+    glm::mat4 pyramidModel = glm::mat4(1.0f);
+    pyramidModel = glm::translate(lightModel, lightPos);
+
+    shaderProgram.Activate();
+    glUniformMatrix4fv(glGetUniformLocation(shaderLight.ID, "model"), 1, GL_FALSE, glm::value_ptr(pyramidModel));
+    shaderLight.Activate();
+    glUniformMatrix4fv(glGetUniformLocation(shaderLight.ID, "model"), 1, GL_FALSE, glm::value_ptr(lightModel));
+
+
+
+
+
+
+
 
     GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
     //GLuint vId = glGetUniformLocation(shaderProgram.ID, "velocity");
@@ -112,7 +197,8 @@ int main()
 
     
 
-    Camera camera(width, height, glm::vec3(0.0f, 1.0f, 2.0f));
+    Camera camera(width, height, glm::vec3(0.0f, 0.0f, 3.0f));
+    glfwSetScrollCallback(window, scroll_callback);
     // glfwSetKeyCallback();
 
     while (!glfwWindowShouldClose(window))
@@ -131,16 +217,23 @@ int main()
         //     angle+=0.5f;
         //     prev_time = currentTime;
         // }
-        camera.input_callback(window);
-        camera.Matrix(45.0f, 0.1f, 100.0f, shaderProgram, "camera");
+        camera.Inputs(window);
+        camera.updateMatrix(fov, 0.1f, 100.0f);
+
+
+        camera.Matrix(shaderProgram, "camera");
 
         glUniform1f(uniID, 0.0);
         tex.Bind();
 
-        
         VAO1.Bind();
         
-        glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(int), GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, sizeof(pyramidIndices)/sizeof(int), GL_UNSIGNED_INT, 0);
+
+        shaderLight.Activate();
+        camera.Matrix(shaderLight, "camera");
+        VAO2.Bind();
+        glDrawElements(GL_TRIANGLES, sizeof(lightIndices)/sizeof(int), GL_UNSIGNED_INT, 0);
 
 
         glfwSwapBuffers(window);
